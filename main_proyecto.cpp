008454d0ad8e2e92baa090cce1080814837bc6ad
GerardoLSJ <lsjgerardo@gmail.com>
2019-05-09 09:42:36 -0500

    changes

--- main_proyecto.cpp
+++ main_proyecto.cpp
@@ -1,7 +1,9 @@
 /*---------------------------------------------------------*/
-/* ----------------   Práctica 11 --------------------------*/
 /*-----------------    2019-2   ---------------------------*/
-/*----------- Alumno: Karen Abril Robles Uribe -------------*/
+/*----------- Alumnos: 
+					Lopez Santibanez Jimenez Luis Gerardo
+					Silva Garcia Carlos Sebastian
+					Robles Uribe Karen Abril 		-------------*/
 /*----------- Proyecto final -------------*/
 //#define STB_IMAGE_IMPLEMENTATION
 #include <glew.h>
@@ -113,7 +115,7 @@ unsigned int	t_azulejo,
 				t_pizarr,
 				t_bend,
 				t_cortina,
-
+				t_cloud,
 				t_panda;
 
 //Keyframes
@@ -290,8 +292,9 @@ void LoadTextures()
 	t_ghost = generateTextures("Texturas/ghost.png", 1);
 	t_hoja = generateTextures("Texturas/hojas.png", 1);
 	t_pizarr = generateTextures("Texturas/pizarr.png", 1);
-	t_bend = generateTextures("Texturas/mach.png", 0);
+		t_bend = generateTextures("Texturas/mach.png", 0);
 	t_cortina = generateTextures("Texturas/cortina.png", 1);
+	t_cloud = generateTextures("Texturas/cloud_b.png", 1);
 
 	t_panda = generateTextures("Texturas/Panda.png", 0);
 
@@ -337,8 +340,11 @@ void LoadTextures()
 
 	glActiveTexture(GL_TEXTURE12);
 	glBindTexture(GL_TEXTURE_2D, t_cortina);
-
+    
 	glActiveTexture(GL_TEXTURE13);
+	glBindTexture(GL_TEXTURE_2D, t_cloud);
+
+	glActiveTexture(GL_TEXTURE14);
 	glBindTexture(GL_TEXTURE_2D, t_panda);
 }
 
@@ -550,8 +556,8 @@ void animate(void)
 	switch (bee_flag)
 	{
 	case 1:
-		bee_x += 0.2f;
-		if (bee_x >= 20)
+		bee_x += 0.05f;
+		if (bee_x >= 5)
 			bee_flag = 2;
 		break;
 	case 2:
@@ -560,7 +566,7 @@ void animate(void)
 			bee_flag = 3;
 		break;
 	case 3:
-		bee_x -= 0.2f;
+		bee_x -= 0.05f;
 		if (bee_x <= 0)
 			bee_flag = 4;
 		break;
@@ -1797,9 +1803,20 @@ void display(Shader shader, Model botaDer, Model piernaDer, Model piernaIzq, Mod
 
 	// hojas animacion remolino
 	model = glm::mat4(1.0f);
-
-	model = glm::translate(model, glm::vec3(0.0f + cc_x * 0.01, 1.0f + cc_y * 0.01, 10.0f + cc_z * 0.01));
+	model = glm::translate(model, glm::vec3(0.0f, 1.0f, 10.0f));
 	model = glm::scale(model, glm::vec3(2.0f, 1.0f, 0.18f));
+	//hojas 1
+	model = glm::translate(model, glm::vec3(cc_x * 0.01, cc_y * 0.01, cc_z * 0.01));
+	lightingShader.setMat4("model", model);
+	lightingShader.setInt("material_diffuse", t_hoja);
+	glDrawArrays(GL_TRIANGLES, 0, 36);
+	//hojas 2
+	model = glm::translate(model, glm::vec3(0.3f + cc_x * 0.01, cc_y * 0.01, -0.3f + cc_z * 0.01));
+	lightingShader.setMat4("model", model);
+	lightingShader.setInt("material_diffuse", t_hoja);
+	glDrawArrays(GL_TRIANGLES, 0, 36);
+	//hojas 3
+	model = glm::translate(model, glm::vec3(-0.6f + cc_x * 0.01, 0.1f + cc_y * 0.01, 0.2f + cc_z * 0.01));
 	lightingShader.setMat4("model", model);
 	lightingShader.setInt("material_diffuse", t_hoja);
 	glDrawArrays(GL_TRIANGLES, 0, 36);
@@ -1808,12 +1825,24 @@ void display(Shader shader, Model botaDer, Model piernaDer, Model piernaIzq, Mod
 	//clouds 
 
 	model = glm::mat4(1.0f);
-	model = glm::translate(model, glm::vec3(0.0f + bee_x , 10.0f + bee_y, 10.0f + bee_z ));
-	model = glm::scale(model, glm::vec3(4.0f, 1.0f, 0.18f));
+	model = glm::translate(model, glm::vec3(-10.0f , 15.0f , 10.0f  ));
+	model = glm::scale(model, glm::vec3(4.0f, 1.0f, 1.0f));
+	//cloud 1
+	model = glm::translate(model, glm::vec3(bee_x, bee_y, bee_z));
 	lightingShader.setMat4("model", model);
-	lightingShader.setInt("material_diffuse", t_hoja);
+	lightingShader.setInt("material_diffuse", t_cloud);
 	glDrawArrays(GL_TRIANGLES, 0, 36);
-
+	//cloud2
+	model = glm::translate(model, glm::vec3( bee_x - 0.8f, bee_y - 0.8f,  bee_z - 0.8f));
+	lightingShader.setMat4("model", model);
+	lightingShader.setInt("material_diffuse", t_cloud);
+	glDrawArrays(GL_TRIANGLES, 0, 36);
+	//cloud3
+	model = glm::translate(model, glm::vec3(-bee_x - 0.5f, -bee_y - 0.5f, -bee_z - 0.5f));
+	lightingShader.setMat4("model", model);
+	lightingShader.setInt("material_diffuse", t_cloud);
+	glDrawArrays(GL_TRIANGLES, 0, 36);
+    
 	//Light
 	lampShader.use();
 	lampShader.setMat4("projection", projection);
@@ -1930,7 +1959,9 @@ void display(Shader shader, Model botaDer, Model piernaDer, Model piernaIzq, Mod
 	
 	 model = glm::translate(model, glm::vec3(posX, 7.0f + posY, posZ));
 	//model = glm::translate(model, glm::vec3(0.0f, 0.0f, 10.0f));
-	model = glm::scale(model, glm::vec3(0.01f, 0.01f,0.01f));
+	model = glm::scale(model, glm::vec3(0.5f, 0.5f, 0.5f));
+	model = tmp = glm::rotate(model, glm::radians(-90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+
 	shader.setMat4("model", model);
 	paper.Draw(shader);	// grass
 
@@ -2003,7 +2034,7 @@ int main()
 	Model pisoModel = ((char*)"Models/piso/piso.obj");
 	Model pcModel = ((char*)"Models/g.obj");
 	Model grassModel = ((char*)"Models/grass2.obj");
-	Model paperModel = ((char*)"Models/g.obj");
+	Model paperModel = ((char*)"Models/abeja/bee.obj");
 	
 	//Inicialización de KeyFrames
 
